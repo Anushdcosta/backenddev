8b363a17600f2569e48fe109ea94a06971a14c76
Anushdcosta <Anushviston@gmail.com>
2024-08-11 16:00:37 +0400

    main head page and header and background designs uploaded

--- capstone/app.js
+++ capstone/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require("mongoose");
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
@@ -13,9 +14,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get("/create", (req, res) => {
+  res.render("create")
+})
+
 
 // Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const port = 3000;
+const dburi =
+  "mongodb+srv://anush:anush123@cluster0.pqnqxux.mongodb.net/nodetuts?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(dburi)
+  .then((results) =>
+    app.listen(process.env.Port || port, () =>
+      console.log(`listening on port ${port}`)
+    )
+  )
+  .catch((err) => console.log(err));
